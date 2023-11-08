c1b14c952bf1b15e7b7d3acc497866d01e1fbfb0
shero121 <keithsherwin6764@gmail.com>
2023-11-08 10:27:19 +0000

    part 1 seems to be right

--- Team.php
+++ Team.php
@@ -1,14 +1,26 @@
 <?php
 class Team {
-    private $name="";
+    private $name = "" ;
     private $totalPoints = 0;
     private $totalGames = 0;
     private $totalGoals = 0;
 
-    public function __construct($parm1) {
-        $this->name = $parm1;
+    public function __construct($name) {
+        $this->name = $name;
     }
 
-    
+    public function finalScore($homeTeamScore, $opposingTeamScore) {
+        $this->totalGames++;
+        $this->totalGoals += $homeTeamScore;
+
+        if ($homeTeamScore > $opposingTeamScore) {
+            $this->totalPoints += 3;
+        } elseif ($homeTeamScore == $opposingTeamScore) {
+            $this->totalPoints += 1;
+        }
+
+
+    }
 }
 ?>
+
