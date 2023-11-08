695d8890cfd1c1b72b11f73738c9e8c9fe3b89a8
shero121 <keithsherwin6764@gmail.com>
2023-11-08 10:12:52 +0000

    right direction

---
+++ Team.php
@@ -0,0 +1,14 @@
+<?php
+class Team {
+    private $name="";
+    private $totalPoints = 0;
+    private $totalGames = 0;
+    private $totalGoals = 0;
+
+    public function __construct($parm1) {
+        $this->name = $parm1;
+    }
+
+    
+}
+?>
